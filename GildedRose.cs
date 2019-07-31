00fdf00e973ef52d8f4cb782f710308627f945fc
Pepijn Schoen <pepijn@eliandor.com>
2019-07-31 17:09:35 +0200

    Extract requirements as unit tests, small refactoring to treat the for loop less awkwardly

--- GildedRose.cs
+++ GildedRose.cs
@@ -4,6 +4,11 @@ namespace csharpcore
 {
     public class GildedRose
     {
+        public static readonly string AGED_BRIE = "Aged Brie";
+        public static readonly string SULFURAS = "Sulfuras, Hand of Ragnaros";
+        public static readonly string BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+        public static readonly string DEFAULT_ITEM = "A piece of thread";
+
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -12,74 +17,74 @@ namespace csharpcore
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach(var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != AGED_BRIE && item.Name != BACKSTAGE_PASS)
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (item.Name != SULFURAS)
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            item.Quality = item.Quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Name == BACKSTAGE_PASS)
                         {
-                            if (Items[i].SellIn < 11)
+                            if (item.SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (item.SellIn < 6)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Name != SULFURAS)
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    item.SellIn = item.SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if (item.SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (item.Name != AGED_BRIE)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Name != BACKSTAGE_PASS)
                         {
-                            if (Items[i].Quality > 0)
+                            if (item.Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (item.Name != SULFURAS)
                                 {
-                                    Items[i].Quality = Items[i].Quality - 1;
+                                    item.Quality = item.Quality - 1;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            item.Quality = item.Quality - item.Quality;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
+                        if (item.Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -5,13 +5,73 @@ namespace csharpcore
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        private void TestItemChange(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = sellIn, Quality = quality } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal(expectedSellIn, Items[0].SellIn);
+            Assert.Equal(expectedQuality, Items[0].Quality);
+        }
+
+        [Fact]
+        public void DefaultItem_DecreasesInSellInAndQuality()
+        {
+            TestItemChange(GildedRose.DEFAULT_ITEM, 5, 10, 4, 9);
+        }
+
+        [Fact]
+        public void DefaultItem_QualityIsNeverNegative()
+        {
+            TestItemChange(GildedRose.DEFAULT_ITEM, 5, 0, 4, 0);
+        }
+
+        [Fact]
+        public void DefaultItem_DegradesAtDoubleSpeedAfterSellInExpired()
+        {
+            TestItemChange(GildedRose.DEFAULT_ITEM, -2, 10, -3, 8);
+        }
+
+        [Fact]
+        public void AgedBrie_IncreasesInQuality()
+        {
+            TestItemChange(GildedRose.AGED_BRIE, 5, 10, 4, 11);
+        }
+
+        [Fact]
+        public void AgedBrie_DoesntPass50AsQuality()
+        {
+            TestItemChange(GildedRose.AGED_BRIE, 5, 50, 4, 50);
+        }
+
+        [Fact]
+        public void Sulfuras_DoesntChangeInQualityOrSellIn()
+        {
+            TestItemChange(GildedRose.SULFURAS, 5, 20, 5, 20);
+        }
+
+        [Fact]
+        public void BackstagePass_IncreasesByOne()
+        {
+            TestItemChange(GildedRose.BACKSTAGE_PASS, 20, 20, 19, 21);
+        }
+
+        [Fact]
+        public void BackstagePass_IncreasesByTwoTenDaysBeforeSellIn()
+        {
+            TestItemChange(GildedRose.BACKSTAGE_PASS, 10, 20, 9, 22);
+        }
+
+        [Fact]
+        public void BackstagePass_IncreasesByThreeFiveDaysBeforeSellIn()
+        {
+            TestItemChange(GildedRose.BACKSTAGE_PASS, 5, 20, 4, 23);
+        }
+
+        [Fact]
+        public void BackstagePass_DropsToZeroAfterSellIn()
+        {
+            TestItemChange(GildedRose.BACKSTAGE_PASS, 0, 20, -1, 0);
         }
     }
 }
